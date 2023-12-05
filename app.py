66f201e88eebba0442c365af27d6628f4e69c5ff
thilinakavirathne <thilinakavirathne@gmail.com>
2023-12-05 05:57:09 +0530

    add Sign Up nd Login Oprataion

--- app.py
+++ app.py
@@ -1,46 +1,39 @@
 import sqlite3
-from flask import Flask, render_template , request, url_for, flash, redirect
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from werkzeug.exceptions import abort
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
+
 def get_post(post_id):
     conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
     conn.close()
     if post is None:
         abort(404)
     return post
 
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'your secret key'
-
-
-
-@app.route('/')
+@app.route('/index')
 def index():
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
     return render_template('index.html', posts=posts)
 
-
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
 
-
-########create method---------------------------------->
-
 @app.route('/create', methods=('GET', 'POST'))
 def create():
-
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
@@ -49,16 +42,12 @@ def create():
             flash('Title is required!')
         else:
             conn = get_db_connection()
-            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
             conn.commit()
             conn.close()
             return redirect(url_for('index'))
-        
+
     return render_template('create.html')
-
-
-###Update--------------------------->
 
 @app.route('/<int:id>/edit', methods=('GET', 'POST'))
 def edit(id):
@@ -72,17 +61,12 @@ def edit(id):
             flash('Title is required!')
         else:
             conn = get_db_connection()
-            conn.execute('UPDATE posts SET title = ?, content = ?'
-                         ' WHERE id = ?',
-                         (title, content, id))
+            conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, id))
             conn.commit()
             conn.close()
             return redirect(url_for('index'))
 
     return render_template('edit.html', post=post)
-
-
-##Delete--------------------->
 
 @app.route('/<int:id>/delete', methods=('POST',))
 def delete(id):
@@ -93,3 +77,43 @@ def delete(id):
     conn.close()
     flash('"{}" was successfully deleted!'.format(post['title']))
     return redirect(url_for('index'))
+
+@app.route('/', methods=('GET', 'POST'))
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if not username or not password:
+            flash('Username and password are required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, password))
+            conn.commit()
+            conn.close()
+            flash('Account created successfully!')
+            return redirect(url_for('login'))
+
+    return render_template('signup.html')
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        conn = get_db_connection()
+        user = conn.execute('SELECT * FROM users WHERE username=? AND password=?', (username, password)).fetchone()
+        conn.close()
+
+        if user:
+            session['username'] = username
+            flash('Login successful!')
+            return redirect(url_for('index'))
+        else:
+            flash('Incorrect username or password.')
+
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- init_db.py
+++ init_db.py
@@ -2,19 +2,25 @@ import sqlite3
 
 connection = sqlite3.connect('database.db')
 
-
+# Read and execute schema.sql to create tables
 with open('schema.sql') as f:
     connection.executescript(f.read())
 
 cur = connection.cursor()
 
+# Insert sample posts
 cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('First Post', 'Content for the first post')
-            )
+            ('First Post', 'Content for the first post'))
 
 cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('Second Post', 'Content for the second post')
-            )
+            ('Second Post', 'Content for the second post'))
+
+# Insert sample users
+cur.execute("INSERT INTO users (username, Ppassword) VALUES (?, ?)",
+            ('user1', ' '))
+
+cur.execute("INSERT INTO users (username, Ppassword) VALUES (?, ?)",
+            ('user2', 'securepass'))
 
 connection.commit()
 connection.close()
